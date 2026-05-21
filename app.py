2b3962684721b285b6196edd3b9d60e945bc069b
hussainmotmi855-a11y <hussainmotmi855@gmail.com>
2026-05-21 06:52:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ def home():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    # جمع البيانات
+    # استخدام .get يوفر حماية من الخطأ إذا كان الحقل فارغاً
     user_data = {
-        "name": request.form.get('name'),
-        "job": request.form.get('job_title'),
+        "name": request.form.get('name', 'غير محدد'),
+        "job": request.form.get('job_title', 'غير محدد'),
+        "objective": request.form.get('objective', ''),
         "experiences": request.form.getlist('experience[]'),
         "skills": request.form.getlist('skills[]')
     }
-    # إرسال البيانات لصفحة التنسيق الاحترافي
     return render_template('result.html', data=user_data)
 
 if __name__ == '__main__':
